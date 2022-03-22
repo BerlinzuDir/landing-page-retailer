832dd719ed3794b10cb472a5bfb73b3568a17240
kilian <kilian.zimmerer@gmail.com>
2022-03-22 12:13:09 +0100

    add content

--- src/Heading.js
+++ src/Heading.js
@@ -4,26 +4,26 @@ import logo from './images/bzd-logo.png';
 export default function Heading() {
   return (
     <div className="row">
-        <div className="col-3 px-4">
-          <img src={logo} className="logo-header mt-5 float-end" alt="logo" />
+      <div className="col-3 px-4">
+        <img src={logo} className="logo-header mt-5 float-end" alt="logo" />
+      </div>
+      <div className="col-9">
+        <div className="row">
+          <div className="col px-0">
+            <h1 className='header text mt-5 fw-bold'>
+              Marktplatz für Berliner Läden
+            </h1>
+          </div>
         </div>
-        <div className="col-9">
-          <div className="row">
-            <div className="col px-0">
-              <h1 className='header text mt-5 fw-bold'>
-                Marktplatz für Berliner Läden
-              </h1>
-            </div>
+        <div className="row">
+          <div className="col mt-3 px-1">
+            <p className='text'>
+              Auf unserem lokalem Online-Marktplatz kannst Du Deine Produkte berlinweit verkaufen. Wir unterstützen Dich bei der Digitalisierung Deines Produktsortiments. Alle Online-Bestellung-en werden noch am selben Tag per Lastenrad abgeholt und an Deine Kunden ausgeliefert.
+            </p>
           </div>
-          <div className="row">
-            <div className="col mt-3 px-1">
-              <p className='text'>
-                Auf unserem lokalem Online-Marktplatz kannst Du Deine Produkte berlinweit verkaufen. Bestellungen werden noch am selben Tag per Lastenrad abgeholt und ausgeliefert.
-              </p>
-            </div>
-            <div className="col-3"></div>
-          </div>
+          <div className="col-3"></div>
         </div>
       </div>
+    </div>
   )
 }
